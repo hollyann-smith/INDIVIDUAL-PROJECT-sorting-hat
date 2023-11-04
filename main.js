32bffcf77ca63c7fad52e8cc4031faecd826a80f
Holly Smith <hollyschuldt@gmail.com>
2023-11-03 19:51:45 -0500

    finished project

--- main.js
+++ main.js
@@ -15,13 +15,13 @@ const renderToDom = (array) => {
 
 array.map((object, index) => {
   domString += 
-      `<div class="card mb-3"  style="max-width: 340px;">
+      `<div id="newkid" class="card mb-3"  style="max-width: 340px;">
         <div class="row g-0">
           <div class="col-md-4" id="${object.house}">
           </div>
           <div class="col-md-8">
             <div class="card-body" >
-              <h5 class="card-title" id ="nameCard">${object.name}</h5>
+              <h5 class="card-titles" id ="nameCard">${object.name}</h5>
               <p class="card-text">${object.house.toUpperCase()}</p>
               <button type ="button" class="btn btn-danger" id="expel--${index}" >EXPEL</button>
             </div>
@@ -44,8 +44,6 @@ form.addEventListener('submit',  (event) => {
       house: classPlacement[Math.floor(Math.random() * classPlacement.length)],
       studentId: Math.floor(Math.random() * (999 - 100) ) + 100
       }
-    
-
     students.push(newStudentObj)
     renderToDom(students)
     form.reset()
@@ -61,14 +59,13 @@ const renderToJail = (array) => {
 
   array.map((object, index) => {
     domStringJail += 
-        `<div class="card m-1" style="width: 15rem;" id="${index}">
+        `<div id="darkside" class="card m-1" style="width: 15rem;" id="${index}">
         <img src="https://www.looper.com/img/gallery/dumbledores-history-with-voldemort-explained/l-intro-1641410297.jpg" alt="Death Eater">
         <div class="card-body text-center">
-          <p class="h6 card-text text-dark">Unfortunately, <span class="text-danger bold">${object.name}</span> went over to the dark side</p>
+          <p class="h6 card-text text-dark">Oh dang... <span class="text-danger bold">${object.name}</span> joined the dark side!</p>
         </div>
       </div>`
       })
-      
       jail.innerHTML = domStringJail
   }
 
@@ -80,21 +77,16 @@ const expelStudent = document.querySelector("#app")
 
 expelStudent.addEventListener("click", (e) => {
   if(e.target.id.includes("expel")){
-
     const [,index] = e.target.id.split("--");
-    
-    expelledGroup.push(students[index])
-    students.splice(index, 1)
-    
+      expelledGroup.push(students[index])
+      students.splice(index, 1)
     renderToDom(students)
     renderToJail(expelledGroup)
   }
-  
 })
 
 //filter function
 const filter = (house) => {
-
   const filteredArray = []
   for(object of students){
       if(object.house === house){
